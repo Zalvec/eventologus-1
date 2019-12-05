3e7c8e7403e77dcf37a41dede9515032a8f6e276
nathanziarczyk <nathanziarczyk1997@outlook.com>
2019-12-05 15:21:19 +0100

    Veranderingen 5 december 2019

--- contact.php
+++ contact.php
@@ -7,33 +7,5 @@ if (!isset($_SESSION["user"])){
     print LoadTemplate("user_header");
 }
 
-?>
-<body>
-    <main class="container">
-        <section class="contcontainerform">
-            <section class="contactform">
-                <h1>Stel ons een vraag</h1>
-                <p>Wil jij ons contacteren? Vul het onderstaande formulier in en wij antwoorden zo snel mogelijk.</p>
-                <form action="contact.php">
-                    <div class="invul">
-                    <label for="naam">Naam</label>
-                    <input type="text" id="naam" name="naam">
-                    </div>
-                    <div class="invul">
-                    <label for="email">Emailadres</label>
-                    <input type="email" id="email" name="email">
-                    </div>
-                    <div class="invul">
-                    <label for="vraag">Vraag</label>
-                        <textarea name="vraag" id="vraag" cols="30" rows="10"></textarea>
-                    </div>
-                    <div class="button">
-                        <button type="submit">Stel je vraag</button>
-                    </div>
-                </form>
-            </section>
-        </section>
-    </main>
-</body>
-<?php
+print LoadTemplate("contact");
 print LoadTemplate("basic_footer");
