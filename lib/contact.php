e301f5cdfd3005958dab556abdb004d9b3719299
nathanziarczyk <nathanziarczyk1997@outlook.com>
2019-12-09 13:12:48 +0100

    Gegevens contactformulier worden doorgestuurd naar de database.

---
+++ lib/contact.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'autoload.php';
+
+$form_name = $_POST['formname'];
+$button = $_POST['submit'];
+
+$naam = "'".$_POST['naam']."'";
+$voornaam = "'".$_POST['voornaam']."'";
+$email = "'".$_POST['email']."'";
+$vraag = "'".$_POST['vraag']."'";
+
+if ($form_name == 'contact_form' AND $button == 'submit'){
+    $sql = "INSERT INTO contact (con_naam, con_voornaam, con_email, con_vraag) VALUES
+            ($naam, $voornaam, $email, $vraag)";
+    if (ExecuteSQL($sql)){
+        $_SESSION['msg'] = 'Bedankt voor uw vraag, wij beantwoorden deze zo snel mogelijk!';
+        header('Location: ../index.php');
+    } else{
+        $_SESSION['msg'] = 'Er ging iets fout bij het verwerken van uw vraag, probeer opnieuw.';
+        header('Location: ../contact.php');
+    }
+}
