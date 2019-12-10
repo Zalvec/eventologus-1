7b1c66b6a5dc205572ed80e3abe086a291ae6098
nathanziarczyk <nathanziarczyk1997@outlook.com>
2019-12-10 17:33:07 +0100

    Aanmaken zonder hyperlinks

--- lib/aanmaken.php
+++ lib/aanmaken.php
@@ -1,8 +1,6 @@
 <?php
 require_once "autoload.php";
 
-include "lib/pdo.php";
-
 $formname = $_POST["formname"];
 $tablename = $_POST["tablename"];
 $pkey = $_POST["pkey"];
@@ -23,14 +21,16 @@ if ($formname == "eve_form" AND $_POST['aanmaakbutton'] == "Aanmaken") {
     $sql_loc = "INSERT INTO locatie SET " .
         "loc_gebouw='" . $_POST['loc_gebouw'] . "' , " .
         "loc_straat='" . $_POST['loc_straat'] . "' , " .
-        "loc_nr='" . $_POST['loc_nr'] . "';" .
-        "loc_pos_id='" . $_POST['loc_pos_id'] . "';";
+        "loc_nr='" . $_POST['loc_nr'] . "' ," .
+        "loc_pos_id='" . $_POST["loc_pos_id"] . "';";
 
-    $sql = "select last_insert_id() laatste_id";
-
+    $loc_id = GetData_LastID($sql_loc)['last_id'];
+    var_dump($_POST);
 
     $sql_eve = "INSERT INTO $tablename SET " .
         "eve_naam='" . $_POST['eve_naam'] . "' , " .
+        "eve_loc_id='" . $loc_id . "' , " .
+        "eve_use_id='" . $_SESSION['user']["use_id"] . "' , " .
         "eve_minprijs='" . $_POST['eve_minprijs'] . "' , " .
         "eve_maxprijs='" . $_POST['eve_maxprijs'] . "' , " .
         "eve_begindatum='" . $_POST['eve_begindatum'] . "' , " .
@@ -39,9 +39,16 @@ if ($formname == "eve_form" AND $_POST['aanmaakbutton'] == "Aanmaken") {
         "eve_sluiting='" . $_POST['eve_sluiting'] . "' , " .
         "eve_image='" . $imagename . "' , " .
         "eve_beschrijving='" . $_POST['eve_beschrijving'] . "' ; ";
-    var_dump(GetData($sql_loc));
-    var_dump(GetData($sql));
-    var_dump(GetData($sql_eve));
+
+    $eve_id = GetData_LastID($sql_eve)['last_id'];
+
+    $sql_cat = "INSERT INTO categorie_evenement SET " .
+        "cev_cat_id='" . $_POST["cev_cat_id"] . "' , " .
+        "cev_eve_id='" . $eve_id . "';";
+
+    print $sql_cat;
+
+    GetData($sql_cat);
 }
 
 
--- lib/pdo.php
+++ lib/pdo.php
@@ -1,17 +1,17 @@
 <?php
 function GetConnection()
 {
-//    $dsn = "mysql:host=185.115.218.166;dbname=wdev_nathan";
-//    $user = "wdev_nathan";
-//    $passwd = "7keQALHp64hZ";
+    $dsn = "mysql:host=185.115.218.166;dbname=wdev_nathan";
+    $user = "wdev_nathan";
+    $passwd = "7keQALHp64hZ";
 
 //    $dsn = "mysql:host=185.115.218.166;dbname=wdev_roel";
 //    $user = "wdev_roel";
 //    $passwd = "xlplnmvEQATn";
-
-    $dsn = "mysql:host=localhost;dbname=eventologus";
-    $user = "root";
-    $passwd = "ArtHur17";
+//
+//    $dsn = "mysql:host=localhost;dbname=eventologus";
+//    $user = "root";
+//    $passwd = "ArtHur17";
 
     $pdo = new PDO($dsn, $user, $passwd);
     return $pdo;
@@ -22,11 +22,27 @@ function GetData( $sql )
     $pdo = GetConnection();
 
     $stm = $pdo->query($sql);
-    $stm->execute();
+    //$stm->execute();
 
     $rows = $stm->fetchAll(PDO::FETCH_ASSOC);
 
-    $last_id = $stm->lastInsertId();
+
+
+    return $rows;
+}
+
+function GetData_LastID( $sql )
+{
+    $pdo = GetConnection();
+
+    $stm = $pdo->query($sql);
+    //$stm->execute();
+
+    $rows = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+
+    $last_id = $pdo->lastInsertId();
+    $rows['last_id'] = $last_id;
 
     return $rows;
 }
