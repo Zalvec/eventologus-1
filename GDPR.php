5e80b87f8c4e2e2c75a4b784fb8323a3ffe5da2e
Roel <roel.van.bilzen@gmail.com>
2019-12-19 12:59:16 +0100

    GDPR en algemene voorwaarden toegevoegd

---
+++ GDPR.php
@@ -0,0 +1,14 @@
+<?php
+require_once "lib/autoload.php";
+?>
+    <h2 class="voorwaarden">GDPR modaliteiten</h2>
+    <br>Eventologus verbindt zich ertoe geen e-mail of marketing acties op te zetten met uitzondering van een informatieve mail op vraag van de prospect tijdens de belactie. Deze vraag om meer informatie moet duidelijk en onomslachtig te interpreteren zijn.
+    <br> <br>Eventologus neemt enkel telefonisch contact op met prospecten die voor ontvangst van de database reeds gecrosscheckt zijn met de Do-Not-Call-Me lijst.
+    <br> <br>Alle klachten, opmerkingen of opt-outs zullen door Eventologus in de desbetreffende database worden geregistreerd en lokaal in het GDPR datalog als wordt de klant binnen de 24uur op de hoogte gebracht hiervan.
+    <br><br>Indien Televantas bvba een datalek ontdekt zal het binnen de wettelijk verplichte 72 uur zowel klant als de officiële instanties hiervan op de hoogte brengen.
+    <br><br>Eventologus legt een bewaartermijn van 3 jaar op alle databases en persoonsgegevens, deze termijn gaat in na afloop van de laatste overeenkomst.
+    <br><br>Eventologus is niet aansprakelijk voor ontvangen data, deze blijft te allen tijde eigendom van de klant. Deze data kunnen we in 2 categorieën onderscheiden; data rechtstreeks aangeleverd door de klant of data die Eventologus aankoopt via externe kanalen en doorfactureert aan de klant waardoor deze bijgevolg eigenaar wordt van de data.
+<?php
+print LoadTemplate("scroll_to_top");
+print LoadTemplate("basic_footer");
+?>
